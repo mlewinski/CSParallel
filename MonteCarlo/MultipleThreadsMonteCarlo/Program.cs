09dd1c15757c5bb85e2c76e476a7b691b3373cd2
Marek Lewiński <lewinski.marek@outlook.com>
2016-07-10 15:35:30 +0200

    Refactor

--- MonteCarlo/MultipleThreadsMonteCarlo/Program.cs
+++ MonteCarlo/MultipleThreadsMonteCarlo/Program.cs
@@ -11,19 +11,27 @@ namespace MultipleThreadsMonteCarlo
     {
         static Random r = new Random();
         const int threadCount = 8;
+        static double pi = 0;
         static void Main(string[] args)
         {
             Thread[] tt = new Thread[threadCount];
             for(int i = 0; i<threadCount; i++)
             {
                 tt[i] = new Thread(RunPiComputation);
-                tt[i].Priority = ThreadPriority.BelowNormal;
+                tt[i].Priority = ThreadPriority.AboveNormal;
                 tt[i].Start();
             }
+            foreach(Thread t in tt)
+            {
+                t.Join();
+                Console.WriteLine("Finished MTID:{0}", t.ManagedThreadId);
+            }
+            Console.WriteLine("PI = {0}, reference = {2}, error = {1}", pi / threadCount, Math.Abs(Math.PI - (pi / threadCount)), Math.PI);
             Console.ReadLine();
         }
         static double CalculatePi(long attemptCount)
         {
+            Random r = new Random(Program.r.Next() & DateTime.Now.Millisecond);
             double x, y;
             long hitCount = 0;
             for (long i = 0; i < attemptCount; i++)
@@ -42,11 +50,11 @@ namespace MultipleThreadsMonteCarlo
             {
                 //int startTime = Environment.TickCount;
                 //int endTime = 0;
-                Random r = new Random(Program.r.Next() & DateTime.Now.Millisecond);
                 long attemptCount = 1000000000L/threadCount;
                 Console.WriteLine("RunPiComputation MTID:{0}", Thread.CurrentThread.ManagedThreadId);
 
                 double pi = CalculatePi(attemptCount);
+                lock ((object)Program.pi) Program.pi += pi;
                 Console.WriteLine("Pi = {0}, computation error = {1}", pi, Math.Abs(Math.PI - pi));
 
                 //endTime = Environment.TickCount;
