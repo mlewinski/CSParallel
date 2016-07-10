18791abf77ee1082647e4caa06bf560f5623d626
Marek Lewiński <lewinski.marek@outlook.com>
2016-07-10 21:06:31 +0200

    Added interlocked atomic operations

--- MonteCarlo/MultipleThreadsMonteCarlo/Program.cs
+++ MonteCarlo/MultipleThreadsMonteCarlo/Program.cs
@@ -10,29 +10,42 @@ namespace MultipleThreadsMonteCarlo
     class Program
     {
         static Random _r = new Random();
-        const int ThreadCount = 15;
+        const int ThreadCount = 20;
         static double _pi = 0;
+        static long AttemptCount = 10000000L;
+        static long Hits = 0;
+        static EventWaitHandle[] ewht = new EventWaitHandle[ThreadCount];
+
         static void Main(string[] args)
         {
             WaitCallback threadMethod = RunPiComputation;
             ThreadPool.SetMaxThreads(30, 100);
-            for(int i = 0; i < ThreadCount; i++)
+            for (int i = 0; i < ThreadCount; i++)
             {
+                ewht[i] = new EventWaitHandle(false, EventResetMode.AutoReset);
                 ThreadPool.QueueUserWorkItem(threadMethod, i);
             }
-
-            int availableThreadCount = 0;
-            int allThreadCount = 0;
-            int workingThreadCount = 0;
-            int tmp = 0;
-            do
+            Thread timer = new Thread(() =>
             {
-                ThreadPool.GetAvailableThreads(out availableThreadCount, out tmp);
-                ThreadPool.GetMaxThreads(out allThreadCount, out tmp);
-                workingThreadCount = allThreadCount - availableThreadCount;
-                Console.WriteLine("Threads working : {0}", workingThreadCount);
-                Thread.Sleep(500);
-            } while (workingThreadCount > 0);
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("Progress : {0}/{1}", Interlocked.Read(ref Program.Hits), Program.AttemptCount * ThreadCount);
+                        Thread.Sleep(500);
+                    }
+                }
+                catch (ThreadAbortException)
+                {
+                    Console.WriteLine("Progress : {0}/{1}", Program.Hits, Program.AttemptCount * ThreadCount);
+                }
+            });
+            timer.Priority=ThreadPriority.Highest;
+            timer.IsBackground = true;
+            timer.Start();
+            for (int i = 0; i < ThreadCount; i++) ewht[i].WaitOne();
+            timer.Abort();
+            Thread.Sleep(200);
             Console.WriteLine("PI = {0}, reference = {2}, error = {1}", _pi / ThreadCount, Math.Abs(Math.PI - (_pi / ThreadCount)), Math.PI);
             Console.ReadLine();
         }
@@ -43,6 +56,7 @@ namespace MultipleThreadsMonteCarlo
             long hitCount = 0;
             for (long i = 0; i < attemptCount; i++)
             {
+                Interlocked.Add(ref Program.Hits, 1);
                 x = r.NextDouble();
                 y = r.NextDouble();
                 if (x * x + y * y < 1) ++hitCount;
@@ -55,21 +69,26 @@ namespace MultipleThreadsMonteCarlo
         {
             try
             {
+                int? indeks = parameter as int?;
                 //int startTime = Environment.TickCount;
                 //int endTime = 0;
-                long attemptCount = 1000000000L/ThreadCount;
-                Console.WriteLine("RunPiComputation MTID:{0}", Thread.CurrentThread.ManagedThreadId);
+                long attemptCount = Program.AttemptCount / ThreadCount;
+                Console.WriteLine("RunPiComputation MTID:{0} ATI:{1}", Thread.CurrentThread.ManagedThreadId, indeks);
 
                 double pi = CalculatePi(attemptCount);
                 lock ((object)Program._pi) Program._pi += pi;
-                Console.WriteLine("Pi = {0}, computation error = {1}", pi, Math.Abs(Math.PI - pi));
-
+                Console.WriteLine("Pi = {0}, computation error = {1} MTID:{2} ATI:{3}", pi, Math.Abs(Math.PI - pi), Thread.CurrentThread.ManagedThreadId, indeks);
+                ewht[indeks.Value].Set();
                 //endTime = Environment.TickCount;
                 //Console.WriteLine("Time elapsed : {0}", (endTime - startTime).ToString());
             }
             catch (ThreadAbortException)
             {
                 Console.WriteLine("Thread aborted! MTID:{0}", Thread.CurrentThread.ManagedThreadId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception in MTID:{0} Message : {1}", Thread.CurrentThread.ManagedThreadId, ex.Message);
             }
         }
     }
