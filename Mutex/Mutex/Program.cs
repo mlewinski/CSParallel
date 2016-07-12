a053ef67c4b8a6dfe34b446bbfebc7cf9d833a1f
Marek Lewiński <lewinski.marek@outlook.com>
2016-07-12 20:52:52 +0200

    Created cross-instance mutex example

--- Mutex/Mutex/Program.cs
+++ Mutex/Mutex/Program.cs
@@ -1,15 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Mutex
+namespace MutexTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Aplikacja uruchomiona");
+            Mutex mutex = new Mutex(false, "mutextest single instance mutex 0x1");
+            Console.WriteLine("Mutex created");
+            Console.WriteLine();
+
+            bool end = false;
+
+            while (true)
+            {
+                mutex.WaitOne();
+                Console.Write('[');
+                if (Console.KeyAvailable)
+                {
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.Enter:
+                            Console.WriteLine();
+                            Console.WriteLine("Thread paused. Press ENTER to release mutex");
+                            Console.ReadLine();
+                            break;
+                        case ConsoleKey.Escape:
+                            end = true;
+                            break;
+                    }
+                }
+
+                mutex.ReleaseMutex();
+                Console.Write(']');
+                if (end)
+                {
+                    Console.WriteLine("End");
+                    return;
+                }
+                Thread.Sleep(1000);
+                Console.Write(" ");
+            }
         }
     }
 }
