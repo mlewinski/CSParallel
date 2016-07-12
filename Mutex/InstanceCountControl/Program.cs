7a6a3e9803738e08b40ccd25583bc773fde3f988
Marek Lewiński <lewinski.marek@outlook.com>
2016-07-12 16:46:49 +0200

    Implemented one-application-instance restriction using Mutex

---
+++ Mutex/InstanceCountControl/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace InstanceCountControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFirstInstance;
+            Mutex mutex = new Mutex(true, "mutex-instancecountcontrol-threading", out isFirstInstance);
+            if (!isFirstInstance)
+            {
+                Console.WriteLine("Only one running instance of application is allowed!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Here goes the application");
+            Console.ReadLine();
+        }
+    }
+}
