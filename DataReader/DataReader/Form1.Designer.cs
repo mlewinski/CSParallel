668090248fbf8113ab5832606a854a643b663d1d
Marek Lewiński <lewinski.marek@outlook.com>
2016-07-30 19:09:28 +0200

    Created BackgroundWorker example of asynchronously adding random integers into listbox

---
+++ DataReader/DataReader/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace DataReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbData = new System.Windows.Forms.ListBox();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.BackgroundWorkerReader = new System.ComponentModel.BackgroundWorker();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start reading";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(197, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Abort";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbData
+            // 
+            this.lbData.FormattingEnabled = true;
+            this.lbData.Location = new System.Drawing.Point(12, 41);
+            this.lbData.Name = "lbData";
+            this.lbData.Size = new System.Drawing.Size(260, 186);
+            this.lbData.TabIndex = 2;
+            // 
+            // Progress
+            // 
+            this.Progress.Location = new System.Drawing.Point(12, 233);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(260, 23);
+            this.Progress.TabIndex = 3;
+            // 
+            // BackgroundWorkerReader
+            // 
+            this.BackgroundWorkerReader.WorkerReportsProgress = true;
+            this.BackgroundWorkerReader.WorkerSupportsCancellation = true;
+            this.BackgroundWorkerReader.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorkerReader_DoWork);
+            this.BackgroundWorkerReader.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorkerReader_ProgressChanged);
+            this.BackgroundWorkerReader.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorkerReader_RunWorkerCompleted);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(93, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(98, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 268);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.lbData);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "DataReader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ListBox lbData;
+        private System.Windows.Forms.ProgressBar Progress;
+        private System.ComponentModel.BackgroundWorker BackgroundWorkerReader;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ DataReader/DataReader/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BackgroundWorkerReader_DoWork(object sender, DoWorkEventArgs e)
+        {
+            const int dataCountToRead = 100;
+            const int msDelayTime = 50;
+            Random r = new Random();
+            for (int i = 0; i < dataCountToRead; i++)
+            {
+                Thread.Sleep(msDelayTime);
+                if (BackgroundWorkerReader.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    BackgroundWorkerReader.ReportProgress(100*i/dataCountToRead,r.Next(255));
+                }
+            }
+        }
+
+        private void BackgroundWorkerReader_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState != null)
+            {
+                lbData.Items.Add(e.UserState);
+                lbData.SelectedIndex = lbData.Items.Count - 1;
+            }
+            Progress.Value = e.ProgressPercentage;
+        }
+
+        private void BackgroundWorkerReader_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ConfigureButtonState(false);
+        }
+
+        private void ConfigureButtonState(bool threadActive)
+        {
+            btnStart.Enabled = !threadActive;
+            btnStop.Enabled = threadActive;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            BackgroundWorkerReader.RunWorkerAsync();
+            ConfigureButtonState(true);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(BackgroundWorkerReader.IsBusy) BackgroundWorkerReader.CancelAsync();
+            ConfigureButtonState(false);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lbData.Items.Clear();
+        }
+    }
+}
