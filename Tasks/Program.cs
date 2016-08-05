0aceb04ecee620143fe78a070e45cff146025240
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-05 21:05:35 +0200

    Implemented TPL Tasks example

---
+++ Tasks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action a = () =>
+            {
+                Console.WriteLine("Task {0} start", Task.CurrentId);
+                Thread.Sleep(new Random().Next(10000));
+                Console.WriteLine("Task {0} finished", Task.CurrentId);
+            };
+
+            List<Task> tasks = new List<Task>();
+            for(int i=0;i<100;i++) tasks.Add(new Task(a));
+
+            tasks.ForEach(t=>t.Start());
+            tasks.ForEach(t=>t.Wait());
+        }
+    }
+}
