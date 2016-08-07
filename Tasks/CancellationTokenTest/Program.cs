428c2e98c52e254c5c383101cec4c04c6e7c2e25
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-07 13:40:47 +0200

    Implemented CancellationToken example

---
+++ Tasks/CancellationTokenTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationTokenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken ct = cts.Token;
+
+            Task t = new Task(() =>
+             {
+                 try
+                 {
+                     Console.WriteLine("Task started");
+                     for (;;) ct.ThrowIfCancellationRequested();
+                 }
+                 catch (OperationCanceledException) { Console.WriteLine("Task canceled"); }
+             }, ct);
+
+            t.Start();
+            Thread.Sleep(3000);
+            cts.Cancel();
+            t.Wait();
+            Console.ReadLine();
+        }
+    }
+}
