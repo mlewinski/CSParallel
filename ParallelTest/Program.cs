70c931989fe903c6c19e34b52afe10b48c785d3e
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-10 16:11:35 +0200

    Implemented parallel divisor finding

---
+++ ParallelTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, (i) => { Console.WriteLine("{0} : {1}", i, Task.CurrentId); });
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PrimeTestParallel/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorsParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 0;
+            while (true)
+            {
+                Console.Write("Input number : ");
+                try
+                {
+                    n = Int64.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                break;
+            }
+
+            List<long> divisors = new List<long>();
+
+            bool prime = true;
+            Parallel.For( 2L, (long) n/2 + 1, (long i) =>
+            {
+                if (n % (long) i == 0)
+                {
+                    Console.WriteLine("{0} is divisible by {1}", n, i);
+                    divisors.Add(i);
+                    prime = false;
+                }
+            });
+            divisors.Sort();
+            if(divisors.Count>0) foreach(long x in divisors) Console.Write(x + " ");
+            if(prime) Console.WriteLine("{0} is prime number", n);
+            Console.ReadLine();
+
+        }
+    }
+}
