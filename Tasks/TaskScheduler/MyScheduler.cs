23a65626119ba0087f841d001a3fe75741c33436
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-10 15:23:58 +0200

    Implemented derived TaskScheduler

---
+++ Tasks/TaskScheduler/MyScheduler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSchedulerTest
+{
+    class MyScheduler : TaskScheduler
+    {
+        private List<Task> queue = new List<Task>();
+        private Thread mainThread;
+
+        public MyScheduler()
+        {
+            mainThread=new Thread(() =>
+            {
+                Console.WriteLine("Scheduler created");
+                while (true)
+                {
+                    if (queue.Count > 0)
+                    {
+                        int i = new Random().Next(queue.Count);
+                        if(TryExecuteTask(queue[i])) queue.RemoveAt(i);
+                    }
+                }
+            });
+            mainThread.IsBackground = true;
+            mainThread.Start();
+        }
+
+        public override int MaximumConcurrencyLevel { get { return 1;} }
+
+        protected override void QueueTask(Task task)
+        {
+            queue.Add(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            Console.WriteLine("Sequential execution call");
+            if (Thread.CurrentThread != mainThread) return false;
+            return TryExecuteTask(task);
+        }
+
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return queue.ToArray();
+        }
+    }
+}
---
+++ Tasks/TaskScheduler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskSchedulerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyScheduler ts = new MyScheduler();
+            TaskFactory tf = new TaskFactory(ts);
+            
+            int maxTasks = 10;
+
+            Task[] tasks = new Task[maxTasks];
+
+
+            for (int i = 0; i < maxTasks; i++)
+            {
+                Console.WriteLine("Task {0} started", i+1);
+                tasks[i] = tf.StartNew((number) =>
+                {
+                    Console.WriteLine("Task {0} finished", number);
+                }, i + 1);
+            }
+            Task.WaitAll(tasks, 20000);
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
