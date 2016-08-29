d0585ed6cdef3c0711d295e7ef21fdf75850d3da
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-29 12:26:49 +0200

    Implemented ConcurrentBag vs List example

---
+++ ConcurrentData/ConcurrentData/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> cb = new ConcurrentBag<int>();
+            List<int> l = new List<int>();
+            Parallel.For(0, 10000, (i) =>
+            {
+                cb.Add(i);
+                l.Add(i);
+            });
+            Console.WriteLine(cb.Count);
+            Console.WriteLine(l.Count);
+            Console.ReadLine();
+        }
+    }
+}
